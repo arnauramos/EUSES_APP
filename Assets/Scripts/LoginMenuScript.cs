159c891a9e9242d9d2dcd42f91231177831b6e8e
arnauramos <arnauramoscomas@gmail.com>
2021-04-26 11:53:17 +0200

    Creada pantalla de login e importado el TMP

---
+++ Assets/Scripts/LoginMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LoginMenuScript : MonoBehaviour
+{
+    public TMP_InputField username;
+    public TMP_InputField password;
+
+    public void onLoginButtonClick()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log(password.text.ToString());
+    }
+}
