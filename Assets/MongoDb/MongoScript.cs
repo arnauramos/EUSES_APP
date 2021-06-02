fc7c549c3b9767a27b7dda7df764e3f4cddd40a4
arnauramos <50210224+arnauramos@users.noreply.github.com>
2021-06-02 13:18:49 +0200

    MongoDB Configurado

---
+++ Assets/MongoDb/MongoScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+
+
+public class MongoScript : MonoBehaviour
+{
+    private const string MONGO_URI = "mongodb+srv://EusesAPP:EusesAPP@cluster0.15ctn.mongodb.net/EusesDB?retryWrites=true&w=majority";
+    private const string DATABASE_NAME = "EusesDB";
+    private MongoClient client;
+    private IMongoDatabase db;
+
+    private void Start()
+    {
+        client = new MongoClient(MONGO_URI);
+        db = client.GetDatabase(DATABASE_NAME);
+
+        insertTestUser();
+
+    }
+
+    private void insertTestUser()
+    {
+        var usersCollection = db.GetCollection<BsonDocument>("Users");
+        var testUser = new BsonDocument { { "Name", "UsuarioTest" }, { "Password", "ContraseñaTest" } };
+        usersCollection.InsertOne(testUser);
+
+        Debug.Log("Usuario insertado");
+    }
+}
