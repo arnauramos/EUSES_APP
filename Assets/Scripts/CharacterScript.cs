b16879e8ee822c8c5e5be847fcacf2b59003d70c
arnauramos <arnauramoscomas@gmail.com>
2021-07-26 22:37:01 +0200

    Rotación de personaje hecha

---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    private Vector3 lastPos, currPos;
+    private float rotationSpeed = -0.2f;
+
+    void Start()
+    {
+        lastPos = Input.mousePosition;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            currPos = Input.mousePosition;
+            Vector3 offset = currPos - lastPos;
+            transform.RotateAround(transform.position, Vector3.up, offset.x * rotationSpeed);
+        }
+        lastPos = Input.mousePosition;
+
+        if (Input.touches.Length > 0)
+        {
+            currPos = Input.touches[0].position;
+            Vector3 offset = currPos - lastPos;
+            transform.RotateAround(transform.position, Vector3.up, offset.x * rotationSpeed);
+            lastPos = Input.touches[0].position;
+        }
+    }
+}
